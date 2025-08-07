3fe4eb0b45819c5195ee92d9f210b5966b77aeea
scarlet-crypto <vanneser21@gmail.com>
2025-08-07 13:19:45 -0400

    crear app

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log ("ejecutando el sistema")
+let = amigos =[];
+let amigoInput = document.querySelector("#amigo");
+let listaDeAmigos = document.querySelector("#listaAmigos");
+let amigoSecreto = document.querySelector("#resultado");
+
+const agregarAmigo = () =>{
+    if (amigo.amigoInput ==""){
+        alert("por favor, ingrese un nombre:");
+
+        }else{
+            amigos.push(amigo.amigoInput.value);
+            console.log (amigos);
+            amigoInput.value ="";
+
+            agregarAmigoLista()
+
+            }
+    }
+
+    const agregarAmigoLista = () =>{
+        listaDeAmigos.innerHTML = "";
+        for (let i = 0; i < amigos.lenth; i ++){
+            listaDeAmigos.innerHTML+='<li>${amigo [i]} </li>';
+        }
+    }
+
+    const sortearAmigo =() =>{
+        if (amigos.lenth === 0){
+            alert("debe ingresar un amigo para iniciar el sorteo");
+
+        }else{
+            let numeroAmigo = Math.floor(Math.random() *
+        amigos.lenth);
+
+        amigoSecreto.textContent = amigos[numeroAmigo];
+    } 
+        }
+
